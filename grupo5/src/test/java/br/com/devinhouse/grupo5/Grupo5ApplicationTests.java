0eacce203f480f0b5fbd3bfb628fe89547d37703
gustavoluchi <gustavo.luchi@gmail.com>
2021-05-17 21:46:18 -0300

    Spring inicializado

---
+++ grupo5/src/test/java/br/com/devinhouse/grupo5/Grupo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devinhouse.grupo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
