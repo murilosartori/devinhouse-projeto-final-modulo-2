839497b20d96dd882da1bc26c5c6fa30a81529e8
gustavoluchi <gustavo.luchi@gmail.com>
2021-05-20 02:56:40 -0300

    criação existsByNuProc.. e um FIXME

--- grupo5/src/main/java/br/com/devinhouse/grupo5/model/ChaveProcesso.java
+++ grupo5/src/main/java/br/com/devinhouse/grupo5/model/ChaveProcesso.java
@@ -1,7 +1,7 @@
 package br.com.devinhouse.grupo5.model;
 
 public class ChaveProcesso {
-
+    //FIXME: essa classe tá sobrando. Querem manter ela? aí eu sugiro colocarmos dentro da classe serviço
     private String sgOrgaoProcesso;
     private String nuProcesso;
     private String nuAnoProcesso;
