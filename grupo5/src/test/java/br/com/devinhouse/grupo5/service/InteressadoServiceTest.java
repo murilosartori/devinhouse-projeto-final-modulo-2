835197c9e91a73192ccebc1c35fdbc7fc2781a70
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2021-05-26 12:08:36 -0300

    bugfix cadastro de interessado

--- grupo5/src/test/java/br/com/devinhouse/grupo5/service/InteressadoServiceTest.java
+++ grupo5/src/test/java/br/com/devinhouse/grupo5/service/InteressadoServiceTest.java
@@ -1,12 +1,14 @@
 package br.com.devinhouse.grupo5.service;
 
 import br.com.devinhouse.grupo5.core.ModelMapperConfig;
+import br.com.devinhouse.grupo5.domain.exceptions.InformacaoJaCadastradaException;
 import br.com.devinhouse.grupo5.domain.exceptions.InteressadoNaoEncontradoException;
 import br.com.devinhouse.grupo5.dto.InteressadoInputDTO;
 import br.com.devinhouse.grupo5.dto.InteressadoOutputDTO;
 import br.com.devinhouse.grupo5.model.Interessado;
 import br.com.devinhouse.grupo5.repository.InteressadoRepository;
 import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,16 +18,19 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.OngoingStubbing;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 
+import java.sql.Date;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.temporal.Temporal;
-import java.util.Date;
+import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -41,9 +46,6 @@ public class InteressadoServiceTest {
     @Mock
     ModelMapper modelMapper;
 
-    @Mock
-    ModelMapperConfig modelMapperConfig;
-
     //
 //    @BeforeEach
 //    public void setup(){
@@ -51,29 +53,105 @@ public class InteressadoServiceTest {
 //        interessadoService = new InteressadoService();
 //    }
 
+
+//    @BeforeEach
+//    void setUp() {
+//        interessadoService = new InteressadoService(interessadoRepository, modelMapper);
+//    }
+
     @Test
-    public void DEVE_RETORNAR_UM_INTERESSADOOUTPUTDTO_QUANDO_DO_CADASTRO_DUM_INTERESSADO(){
+    public void verificarOCadastroDeUmInteressadoComInformacoesValidas(){
 
-        InteressadoInputDTO interessadoInputDTO = new
-                InteressadoInputDTO("Pedro Silva", "12478959984",
-                Date.from(Instant.now()), true);
 
-        Interessado interessado = new Interessado(1L,
-                interessadoInputDTO.getNmInteressado(),
-                interessadoInputDTO.getNuIdentificacao(), interessadoInputDTO.getDtNascimento(), interessadoInputDTO.getFlAtivo()
+//        // given
+////        InteressadoInputDTO interessadoDto = new InteressadoInputDTO( "fulano", "12345654321", Date.valueOf("2002-10-31"), true);
+////        Interessado interessado = new Interessado(1L, interessadoDto.getNmInteressado(), interessadoDto.getNuIdentificacao(), interessadoDto.getDtNascimento(), interessadoDto.getFlAtivo());
+//
+//        InteressadoInputDTO interessadoInputDTO = new InteressadoInputDTO(
+//                "Fulano",
+//                "12478959984",
+//                Date.valueOf("2000-12-12"),
+//                true
+//        );
+//
+//        Interessado interessado = new Interessado(
+//                1L,
+//                "Fulano",
+//                "12478959984",
+//                Date.valueOf("2000-12-12"),
+//                true
+//        );
+//
+//        // when
+////        when(modelMapper.map(interessadoInputDTO, Interessado.class)).thenReturn(interessado);
+////        when(interessadoRepository.save(interessado)).thenReturn(interessado);
+//
+//        when(modelMapper.map(interessadoInputDTO, Interessado.class)).thenReturn(interessado);
+//        when(interessadoRepository.save(interessado)).thenReturn(interessado);
+//
+//        // then
+//        InteressadoOutputDTO interessadoOutputDTO = interessadoService.cadastrarInteressado(interessadoInputDTO);
+//
+//        assertAll(
+//                () -> verify(interessadoRepository).save(interessado)
+//              //  () -> verify(interessadoRepository, times(1)).save(interessado)
+//        );
+
+//        //given
+        InteressadoInputDTO interessadoInputDTO = new InteressadoInputDTO(
+                "Fulano",
+                "12345654321",
+                Date.valueOf("2000-12-12"),
+                true
         );
 
-        InteressadoOutputDTO interessadoOutputDTO = new InteressadoOutputDTO(1L,
-                interessado.getNmInteressado(), interessado.getNuIdentificacao(),
-                interessado.getDtNascimento(), interessado.getFlAtivo());
+        Interessado interessado = new Interessado(
+                1L,
+                "Fulano",
+                "12345654321",
+                Date.valueOf("2000-12-12"),
+                true
+        );
 
+        //when
         when(modelMapper.map(interessadoInputDTO, Interessado.class)).thenReturn(interessado);
+        when(interessadoRepository.save(interessado)).thenReturn(interessado);
 
-        when(modelMapper.map(interessado, InteressadoOutputDTO.class)).thenReturn(interessadoOutputDTO);
+        InteressadoOutputDTO interessadoOutputDTO = interessadoService.cadastrarInteressado(interessadoInputDTO);
 
-        InteressadoOutputDTO outputDTO = interessadoService.cadastrarInteressado(interessadoInputDTO);
+        //then
+       assertAll(
+               () -> verify(interessadoRepository).save(interessado)
+        );
 
-        AssertionsForClassTypes.assertThat(outputDTO).isInstanceOf(InteressadoOutputDTO.class);
+
+
+//        assertThat(catchThrowable(() -> {
+//            interessadoRepository.findByNuIdentificacao(interessado.getNuIdentificacao());
+
+//        })).isInstanceOf(InformacaoJaCadastradaException.class);
+
+
+//        InteressadoInputDTO interessadoInputDTO = new
+//                InteressadoInputDTO("Pedro Silva", "12478959984",
+//                Date.from(Instant.now()), true);
+//
+//        Interessado interessado = new Interessado(1L,
+//                interessadoInputDTO.getNmInteressado(),
+//                interessadoInputDTO.getNuIdentificacao(), interessadoInputDTO.getDtNascimento(), interessadoInputDTO.getFlAtivo()
+//        );
+//
+//        InteressadoOutputDTO interessadoOutputDTO = new InteressadoOutputDTO(1L,
+//                interessado.getNmInteressado(), interessado.getNuIdentificacao(),
+//                interessado.getDtNascimento(), interessado.getFlAtivo());
+//
+//        when(modelMapper.map(interessadoInputDTO, Interessado.class)).thenReturn(interessado);
+//
+//        when(modelMapper.map(interessado, InteressadoOutputDTO.class)).thenReturn(interessadoOutputDTO);
+//
+//        InteressadoOutputDTO outputDTO = interessadoService.cadastrarInteressado(interessadoInputDTO);
+//
+//        AssertionsForClassTypes.assertThat(outputDTO).isInstanceOf(InteressadoOutputDTO.class);
     }
 
     @Test
