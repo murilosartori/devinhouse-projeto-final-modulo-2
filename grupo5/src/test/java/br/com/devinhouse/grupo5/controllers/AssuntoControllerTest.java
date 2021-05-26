26519c41edf18f834a73684b3160f3e09071262b
gustavoluchi <gustavo.luchi@gmail.com>
2021-05-26 20:11:30 -0300

    Esqueleto de Testes

---
+++ grupo5/src/test/java/br/com/devinhouse/grupo5/controllers/AssuntoControllerTest.java
@@ -0,0 +1,22 @@
+package br.com.devinhouse.grupo5.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssuntoControllerTest {
+
+  @Test
+  void cadastrarAssunto () {
+    // Given
+    // When
+    // Then
+  }
+
+  @Test
+  void buscarAssuntoPorId () {
+    // Given
+    // When
+    // Then
+  }
+}
