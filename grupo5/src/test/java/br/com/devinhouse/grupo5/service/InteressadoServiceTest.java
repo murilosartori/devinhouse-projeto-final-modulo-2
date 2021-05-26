c7f760ed4417851ffd2a6b3f64ffd51db20732ea
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2021-05-25 22:36:57 -0300

    fixme: código de teste de cadastro de interessado

--- grupo5/src/test/java/br/com/devinhouse/grupo5/service/InteressadoServiceTest.java
+++ grupo5/src/test/java/br/com/devinhouse/grupo5/service/InteressadoServiceTest.java
@@ -1,40 +1,84 @@
 package br.com.devinhouse.grupo5.service;
 
+import br.com.devinhouse.grupo5.core.ModelMapperConfig;
 import br.com.devinhouse.grupo5.domain.exceptions.InteressadoNaoEncontradoException;
-import br.com.devinhouse.grupo5.dto.AssuntoInputDTO;
-import br.com.devinhouse.grupo5.dto.AssuntoOutputDTO;
 import br.com.devinhouse.grupo5.dto.InteressadoInputDTO;
 import br.com.devinhouse.grupo5.dto.InteressadoOutputDTO;
+import br.com.devinhouse.grupo5.model.Interessado;
+import br.com.devinhouse.grupo5.repository.InteressadoRepository;
 import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.OngoingStubbing;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.temporal.Temporal;
 import java.util.Date;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class InteressadoServiceTest {
 
-    @Autowired
+    @InjectMocks
     InteressadoService interessadoService;
+
+    @Mock
+    InteressadoRepository interessadoRepository;
+
+    @Mock
+    ModelMapper modelMapper;
+
+    @Mock
+    ModelMapperConfig modelMapperConfig;
+
+    //
+//    @BeforeEach
+//    public void setup(){
+//interessadoRepository = mock(InteressadoRepository.class);
+//        interessadoService = new InteressadoService();
+//    }
 
     @Test
     public void DEVE_RETORNAR_UM_INTERESSADOOUTPUTDTO_QUANDO_DO_CADASTRO_DUM_INTERESSADO(){
+
         InteressadoInputDTO interessadoInputDTO = new
-                InteressadoInputDTO("Pedro Silva", "11122233344", Date.from(Instant.now()), true);
+                InteressadoInputDTO("Pedro Silva", "12478959984",
+                Date.from(Instant.now()), true);
 
-        InteressadoOutputDTO interessadoOutputDTO = interessadoService.cadastrarInteressado(interessadoInputDTO);
+        Interessado interessado = new Interessado(1L,
+                interessadoInputDTO.getNmInteressado(),
+                interessadoInputDTO.getNuIdentificacao(), interessadoInputDTO.getDtNascimento(), interessadoInputDTO.getFlAtivo()
+        );
 
-        AssertionsForClassTypes.assertThat(interessadoOutputDTO).isInstanceOf(InteressadoOutputDTO.class);
+        InteressadoOutputDTO interessadoOutputDTO = new InteressadoOutputDTO(1L,
+                interessado.getNmInteressado(), interessado.getNuIdentificacao(),
+                interessado.getDtNascimento(), interessado.getFlAtivo());
+
+        when(modelMapper.map(interessadoInputDTO, Interessado.class)).thenReturn(interessado);
+
+        when(modelMapper.map(interessado, InteressadoOutputDTO.class)).thenReturn(interessadoOutputDTO);
+
+        InteressadoOutputDTO outputDTO = interessadoService.cadastrarInteressado(interessadoInputDTO);
+
+        AssertionsForClassTypes.assertThat(outputDTO).isInstanceOf(InteressadoOutputDTO.class);
     }
 
     @Test
     public void DEVE_RETORNAR_ERRO_QUANDO_BUSCAR_POR_UM_INTERESSADO_PELO_NUIDENTIFICACAO_E_O_MESMO_NAO_ESTIVER_CADASTRADO(){
-        Throwable exception = catchThrowable(() -> interessadoService.buscarInteressadoPeloNuIdentificacao("11122233344"));
+        Throwable exception = catchThrowable(() -> interessadoService.buscarInteressadoPeloNuIdentificacao("11122233355"));
 
         assertThat(exception).isInstanceOf(InteressadoNaoEncontradoException.class);
     }
