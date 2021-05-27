75b56aeaf5d50b392cfde1c2f12a6e49adcc1bd3
gustavoluchi <gustavo.luchi@gmail.com>
2021-05-26 21:06:54 -0300

    Esqueleto do teste do controller

--- grupo5/src/test/java/br/com/devinhouse/grupo5/controllers/AssuntoControllerTest.java
+++ grupo5/src/test/java/br/com/devinhouse/grupo5/controllers/AssuntoControllerTest.java
@@ -1,15 +1,36 @@
 package br.com.devinhouse.grupo5.controllers;
 
+import br.com.devinhouse.grupo5.service.AssuntoService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-class AssuntoControllerTest {
+@WebMvcTest(AssuntoController.class)
+public class AssuntoControllerTest {
+
+  @Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private AssuntoService service;
 
   @Test
-  void cadastrarAssunto () {
+  public void cadastrarAssuntoDeveRetornarHttpCreated () throws Exception {
     // Given
+
     // When
+    this.mockMvc.perform(get("/v1/assunto").content())
+      .andExpect(status().isOk());
+//      .andDo(print())
+//      .andExpect(content().string(containsString("Hello, Mock")));
+  }
     // Then
   }
 
