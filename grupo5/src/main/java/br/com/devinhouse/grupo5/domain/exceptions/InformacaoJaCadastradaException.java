f25f5dbc8f344b3bda692306f8027313670b1799
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2021-05-28 15:08:20 -0300

    Realizados mais alguns testes

--- grupo5/src/main/java/br/com/devinhouse/grupo5/domain/exceptions/InformacaoJaCadastradaException.java
+++
@@ -1,9 +0,0 @@
-package br.com.devinhouse.grupo5.domain.exceptions;
-
-public class InformacaoJaCadastradaException extends RuntimeException{
-
-    public InformacaoJaCadastradaException(String mensagem){
-        super(mensagem);
-    }
-
-}
