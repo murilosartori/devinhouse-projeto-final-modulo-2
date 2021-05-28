dc72d23744ab93835f2884c2efd4adb48e983c86
Murilo Sartori <m.sartori22@gmail.com>
2021-05-27 23:30:31 -0300

    implementar os testes de busca dos processos

--- grupo5/src/test/java/br/com/devinhouse/grupo5/service/ProcessoServiceTest.java
+++ grupo5/src/test/java/br/com/devinhouse/grupo5/service/ProcessoServiceTest.java
@@ -1,64 +1,148 @@
 package br.com.devinhouse.grupo5.service;
 
+import br.com.devinhouse.grupo5.domain.exceptions.ProcessoNaoEncontradoException;
+import br.com.devinhouse.grupo5.dto.AssuntoOutputDTO;
+import br.com.devinhouse.grupo5.dto.InteressadoOutputDTO;
+import br.com.devinhouse.grupo5.model.Assunto;
+import br.com.devinhouse.grupo5.model.Interessado;
+import br.com.devinhouse.grupo5.model.Processo;
+import br.com.devinhouse.grupo5.repository.AssuntoRepository;
+import br.com.devinhouse.grupo5.repository.InteressadoRepository;
+import br.com.devinhouse.grupo5.repository.ProcessoRepository;
+import org.aspectj.util.PartialOrder;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class ProcessoServiceTest {
 
-  @Test
-  void salvarProcesso () {
-    // Given
-    // When
-    // Then
-  }
+	@Mock
+	ProcessoRepository processoRepository;
 
-  @Test
-  void buscarTodosProcessos () {
-    // Given
-    // When
-    // Then
-  }
+	@InjectMocks
+	ProcessoService processoService;
 
-  @Test
-  void buscarUmProcesso () {
-    // Given
-    // When
-    // Then
-  }
+	@Mock
+	ModelMapper modelMapper;
 
-  @Test
-  void buscarUmProcessoPorChave () {
-    // Given
-    // When
-    // Then
-  }
+	@Mock
+	InteressadoService interessadoService;
 
-  @Test
-  void buscarUmProcessoPorCdInteressado () {
-    // Given
-    // When
-    // Then
-  }
+	@Mock
+	AssuntoService assuntoService;
 
-  @Test
-  void buscarUmProcessoPorCdAssunto () {
-    // Given
-    // When
-    // Then
-  }
+	@Test
+	void salvarProcesso() {
 
-  @Test
-  void atualizarProcesso () {
-    // Given
-    // When
-    // Then
-  }
+	}
 
-  @Test
-  void deletarProcesso () {
-    // Given
-    // When
-    // Then
-  }
+	@Test
+	void buscarTodosProcessos() {
+		ArrayList<Processo> processos = new ArrayList<>();
+
+		when(processoRepository.findAll()).thenReturn(processos);
+
+		processoService.buscarTodosProcessos();
+
+		verify(processoRepository, times(1)).findAll();
+	}
+
+	@Test
+	void buscarUmProcesso() {
+		Processo processo = new Processo();
+
+		when(processoRepository.findById(1L)).thenReturn(Optional.of(processo));
+
+		processoService.buscarUmProcesso(1L);
+
+		verify(processoRepository, times(1)).findById(1L);
+	}
+
+	@Test
+	void buscarUmProcessoNaoEncontrado() {
+		Throwable erro = catchThrowable(() -> processoService.buscarUmProcesso(1L));
+
+		assertThat(erro).isInstanceOf(ProcessoNaoEncontradoException.class);
+	}
+
+	@Test
+	void buscarUmProcessoPorChave() {
+		Processo processo = new Processo();
+
+		when(processoRepository.findByChaveProcesso("SOFT 1/2021")).thenReturn(Optional.of(processo));
+
+		processoService.buscarUmProcessoPorChave("SOFT 1/2021");
+
+		verify(processoRepository, times(1)).findByChaveProcesso("SOFT 1/2021");
+	}
+
+	@Test
+	void buscarUmProcessoPorChaveNaoEncontrado() {
+		Throwable erro = catchThrowable(() -> processoService.buscarUmProcessoPorChave("SOFT 1/2020"));
+
+		assertThat(erro).isInstanceOf(ProcessoNaoEncontradoException.class);
+	}
+
+	@Test
+	void buscarUmProcessoPorCdInteressado() {
+		ArrayList<Processo> processos = new ArrayList<>();
+		Processo processo = new Processo();
+		processos.add(processo);
+		Interessado interessado = new Interessado(1L, "Fulano de Tal", "65701715000", LocalDate.parse("2021-05-26"), true);
+
+		InteressadoOutputDTO interessadoOutputDTO = new InteressadoOutputDTO();
+
+		when(interessadoService.buscarInteressadoPeloId(interessado.getId())).thenReturn(interessadoOutputDTO);
+		when(modelMapper.map(interessadoService.buscarInteressadoPeloId(interessado.getId()), Interessado.class))
+				.thenReturn(interessado);
+		when(processoRepository.findByCdInteressado(interessado)).thenReturn(processos);
+
+		processoService.buscarUmProcessoPorCdInteressado(1L);
+
+		verify(processoRepository, times(1)).findByCdInteressado(interessado);
+	}
+
+	@Test
+	void buscarUmProcessoPorCdAssunto() {
+		ArrayList<Processo> processos = new ArrayList<>();
+		Assunto assunto = new Assunto(1L, "Descrição Assunto", LocalDate.parse("2021-05-26"), true);
+		AssuntoOutputDTO assuntoOutputDTO = new AssuntoOutputDTO();
+
+		Processo processo = new Processo();
+
+		when(assuntoService.buscarAssuntoPorId(assunto.getId())).thenReturn(assuntoOutputDTO);
+		when(modelMapper.map(assuntoService.buscarAssuntoPorId(assunto.getId()), Assunto.class)).thenReturn(assunto);
+		when(processoRepository.findByCdAssunto(assunto)).thenReturn(processos);
+
+		processoService.buscarUmProcessoPorCdAssunto(1L);
+
+		verify(processoRepository, times(1)).findByCdAssunto(assunto);
+	}
+
+	@Test
+	void atualizarProcesso() {
+		// Given
+		// When
+		// Then
+	}
+
+	@Test
+	void deletarProcesso() {
+		// Given
+		// When
+		// Then
+	}
 }
