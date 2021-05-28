a4f41192cf1e2d599d8d5d60ce461daf800dedcb
gustavoluchi <gustavo.luchi@gmail.com>
2021-05-28 16:07:49 -0300

    Refatorações

--- grupo5/src/test/java/br/com/devinhouse/grupo5/controllers/InteressadoControllerTest.java
+++ grupo5/src/test/java/br/com/devinhouse/grupo5/controllers/InteressadoControllerTest.java
@@ -1,10 +1,11 @@
 package br.com.devinhouse.grupo5.controllers;
 
-import br.com.devinhouse.grupo5.dto.AssuntoOutputDTO;
 import br.com.devinhouse.grupo5.dto.InteressadoInputDTO;
 import br.com.devinhouse.grupo5.dto.InteressadoOutputDTO;
 import br.com.devinhouse.grupo5.service.InteressadoService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,7 +14,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -24,23 +24,27 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(InteressadoController.class)
 class InteressadoControllerTest {
 
-  private final String BASE_URL = "/v1/interessado";
-
   @Autowired
 	private MockMvc mockMvc;
 
 	@MockBean
 	private InteressadoService service;
 
-  @Autowired
-  private ObjectMapper objectMapper;
-
-
-  InteressadoInputDTO interessadoInput = new InteressadoInputDTO();
-  InteressadoOutputDTO interessadoOutput = new InteressadoOutputDTO();
+  private final String BASE_URL = "/v1/interessado";
+	static private ObjectMapper objectMapper;
+	static private InteressadoInputDTO interessadoInput;
+	static private InteressadoOutputDTO interessadoOutput;
+  static private String interessadoOutputString;
+  @BeforeAll
+  static void setUp() throws JsonProcessingException {
+  objectMapper = new ObjectMapper();
+  interessadoInput = new InteressadoInputDTO();
+  interessadoOutput = new InteressadoOutputDTO();
+  interessadoOutputString = objectMapper.writeValueAsString(interessadoOutput);
+  }
 
   @Test
-  void cadastrarAssuntoDeveRetornarHttpCreated () throws Exception {
+  void cadastrarAssuntoDeveRetornarInteressadoOutputComoJsonEHttpCreated () throws Exception {
     // Given
     MockHttpServletRequestBuilder request = post(BASE_URL)
       .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -50,22 +54,24 @@ class InteressadoControllerTest {
     when(service.cadastrarInteressado(any())).thenReturn(interessadoOutput);
     this.mockMvc.perform(request) // Then
       .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+      .andExpect(content().string(interessadoOutputString))
       .andExpect(status().isCreated());
   }
 
   @Test
-  void buscarInteressadoPeloIdDeveRetornarJsonEHttpOk () throws Exception {
+  void buscarInteressadoPeloIdDeveRetornarInteressadoOutputComoJsonEHttpOk () throws Exception {
     // Given
     MockHttpServletRequestBuilder request = get(BASE_URL+"/id/1");
     // When
     when(service.buscarInteressadoPeloId(1L)).thenReturn(interessadoOutput);
     this.mockMvc.perform(request) // Then
       .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+      .andExpect(content().string(interessadoOutputString))
       .andExpect(status().isOk());
   }
 
   @Test
-  void buscarInteressadoPeloNuIdentificacao () throws Exception {
+  void buscarInteressadoPeloNuIdentificacaoDeveRetornarInteressadoOutputComoJsonEHttpOk () throws Exception {
     // Given
     final String TESTE_DO_PARAMETRO = "testedoparametro";
     MockHttpServletRequestBuilder request = get(BASE_URL+"/nuidentificacao?valor="+TESTE_DO_PARAMETRO);
@@ -73,6 +79,7 @@ class InteressadoControllerTest {
     when(service.buscarInteressadoPeloNuIdentificacao(TESTE_DO_PARAMETRO)).thenReturn(interessadoOutput);
     this.mockMvc.perform(request) // Then
       .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+      .andExpect(content().string(interessadoOutputString))
       .andExpect(status().isOk());
   }
 }
